b4a011c2e919be3edabfcd107a1beab316fe1787
Brett Sheleski <sheleski@fvtc.edu>
2021-10-18 13:05:03 -0500

    Added encoding parameter

--- DelimitedFile.MvcCore/CsvFileResult.cs
+++ DelimitedFile.MvcCore/CsvFileResult.cs
@@ -2,23 +2,32 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Sheleski.DelimitedFile.MvcCore
 {
     public class CsvFileResult : IActionResult
     {
-        public CsvFileResult(CsvFile csvFile, string fileName)
+        public CsvFileResult(CsvFile csvFile, string fileName) : this(csvFile, fileName, Encoding.Default)
+        {
+
+
+        }
+
+        public CsvFileResult(CsvFile csvFile, string fileName, Encoding encoding)
         {
             if (csvFile == null)
                 throw new ArgumentNullException(nameof(csvFile));
 
             this.CsvFile = csvFile;
             this.Filename = fileName;
+            this.Encoding = encoding;
         }
 
         public CsvFile CsvFile { get; }
         public string Filename { get; }
+        public Encoding Encoding { get; }
 
         public async Task ExecuteResultAsync(ActionContext context)
         {
@@ -33,7 +42,7 @@ namespace Sheleski.DelimitedFile.MvcCore
             context.HttpContext.Response.Headers.Add("Pragma", "public");
 
             
-            await using (var writer = new StreamWriter(context.HttpContext.Response.Body))
+            await using (var writer = new StreamWriter(context.HttpContext.Response.Body, this.Encoding))
             {
                 await this.CsvFile.WriteAsync(writer);
             }
